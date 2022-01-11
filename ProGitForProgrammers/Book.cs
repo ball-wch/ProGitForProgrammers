89894b7f0331a6c6eb3cea1adb1bdca9b3e4ba98
ball-wch <worachotn@gmail.com>
2022-01-11 22:59:50 +0700

    Add properties

--- ProGitForProgrammers/Book.cs
+++ ProGitForProgrammers/Book.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace ProGitForProgrammers
 {
-    public string Title { get; set; }
-    public List<string> Authors { get; set; }
-    public DateTime PublicationDate { get; set; }
+    partial class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+    
 }
---
+++ ProGitForProgrammers/Calculator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGitForProgrammers
+{
+    public class Calculator
+    {
+
+    }
+    public int Add(int left, int right)
+    {
+        return left + right;
+    }
+    public int Subtract(int left, int right)
+    {
+        return left - right;
+    }
+    public int Multiply(int left, int right)
+    {
+        return left * right;
+    }
+    public int Divide(int left, int right)
+    {
+        return left / right;
+    }
+}
