2176a1d048e42d8cfa0e6a357c9afd00b011d8d4
ball-wch <worachotn@gmail.com>
2022-01-11 21:37:06 +0700

    Add inoformative line

---
+++ ProGitForProgrammers/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I just added this in Visual Studio");
+        }
+    }
+}
