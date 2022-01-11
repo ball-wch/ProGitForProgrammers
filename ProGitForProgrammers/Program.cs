67d0e5a588c6cc342a2d5bbdfec8228a2648c7ba
ball-wch <worachotn@gmail.com>
2022-01-11 22:22:37 +0700

    Add calculator class

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace ProGitForProgrammers
 {
-    public class Calculator
+    class Program
     {
-
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line added to show the staging area");
+        }
     }
 }
